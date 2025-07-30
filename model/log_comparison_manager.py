9853256ba058bb457e368f7d9d4ac2f7b4d7903a
Jayson Barateta <jayson.b@nova-hub.net>
2025-07-30 23:21:19 +0800

    Updates 730

--- model/log_comparison_manager.py
+++ model/log_comparison_manager.py
@@ -1,5 +1,111 @@
+import re
+import difflib
+from typing import Dict, List
 
+from core.syslogger import logger
+from core.constants import Const
 
 class LogComparisonManager:
-    def __init__(self):
+    @staticmethod
+    def parse_log(network_logs: Dict[str, str]) -> Dict[str, Dict[str, str]]:
+        """Parses multiple device logs into structured command output.
+
+        Args:
+            network_logs: Dict where key = hostname, value = full CLI log string.
+
+        Returns:
+            Dict where:
+                - key = hostname
+                - value = dict of {command: output}
+        """
+        parsed_logs = {}
+        
+        for hostname, logs in network_logs.items():
+            # Pattern to find each hostname#show block
+            pattern = rf"(?={re.escape(hostname)}#show\s+\s+)"
+            sections = re.split(pattern, logs.strip())
+            command_outputs = {}
+            
+            for section in sections:
+                if not section.strip():
+                    continue
+                
+                lines = section.strip().splitlines()
+                
+                if not lines:
+                    continue
+                
+                header = lines[0].replace(f"{hostname}#","").strip()
+                content = "\n".join(lines[1:]).strip()
+                command_outputs[header] = content
+                
+            parsed_logs[hostname] = command_outputs
+        
+        return parsed_logs
+    
+    @classmethod
+    def compare(cls, parsed_pre_logs, parsed_post_logs):
         pass
+    
+    @classmethod
+    def is_junk(cls, word='[TIME]'):
+        return bool(re.match(r"&\d{2}:\d{2}:\d{2}$", word))
+    
+    @classmethod
+    def convert_status(cls, status):
+        return "Normal" if status else "Abnormal"
+    
+    @classmethod
+    def compare_char_by_char(cls, pre_sh_cmd_log, post_sh_cmd_log) -> tuple:
+        line_results = []
+        equal_flag = True
+        
+        pre_log_len = len(pre_sh_cmd_log)
+        post_log_len = len(post_sh_cmd_log)
+        loop_count = pre_log_len if pre_log_len > post_log_len else post_log_len
+        
+        for i in range(loop_count):
+            original = pre_sh_cmd_log[i] if i < len(pre_sh_cmd_log) else ""
+            modified = post_sh_cmd_log[i] if i < len(post_sh_cmd_log) else ""
+            
+            # Compare words between original and modified text
+            matcher = difflib.SequenceMatcher(cls.is_junk, original, modified)
+            opcodes = matcher.get_opcodes()
+            
+            if equal_flag and opcodes and not all(x[0] == "equal" for x in opcodes):
+                equal_flag = False
+                
+            line_results.append({"opcodes": opcodes, "pre_log_line": original, "post_log_line": modified})
+        
+        return line_results, equal_flag
+    
+    @classmethod
+    def compare_line_by_line(cls, prelog: List[str], postlog: List[str]) -> List[str]:
+        sm = difflib.SequenceMatcher(cls.is_junk, prelog, postlog)
+        result = []
+        
+        for tag, i1, i2, j1, j2 in sm.get_opcodes():
+            if tag == "equal":
+                for line in prelog[i1:i2]:
+                    result.append(f"  {line.rstrip()}")
+            
+            elif tag == "replace":
+                for line in prelog[i1:i2]:
+                    if not line.strip():
+                        result.append(f"D: {line.rstrip()}")
+                    
+                for line in postlog[j1:j2]:
+                    if not line.strip():
+                        result.append(f"D: {line.rstrip()}")
+                    else:
+                        result.append(f"C: {line.rstrip()}")
+            
+            elif tag == "delete":
+                for line in prelog[i1:i2]:
+                    result.append(f"D: {line.rstrip()}")
+                    
+            elif tag == "insert":
+                for line in postlog[j1:j2]:
+                    result.append(f"A: {line.rstrip()}")
+                    
+        return result
--- model/log_manager.py
+++ model/log_manager.py
@@ -1,4 +1,15 @@
+import re
+import os
+from pathlib import Path
+from typing import Dict, List, Final
+from netmiko import ConnectHandler
 
+from core.syslogger import logger
+from core.constants import Const
+from core.utility import resource_path
+
+
+from .log_comparison_manager import LogComparisonManager
 
 class LogManager:
     def __init__(self):
@@ -7,29 +18,162 @@ class LogManager:
         self.device_log_stats = {}
         self.comparison_result = {}
     
-    def collect_logs(self):
-        pass
+    def collect_logs(self, device_configs, reachable_devices):
+        if not device_configs:
+            logger.error("No devices available for log collection.")
+            return Const.LOG_COLL_SKIP
+        
+        logger.info(f"Collecting network logs from {len(device_configs)} devices")
+        
+        self.reset_device_log_stats()
+        collection_flag = False
+        
+        for device in device_configs:
+            hostname = device["hostname"]
+            conn_type = device["connection_type"]
+            self.device_log_stats.setdefault(hostname, [])
+            
+            if device not in reachable_devices:
+                continue
+            
+            config = DeviceConfigManager.create_configuration(device)
+            show_commands = device_configs.get_show_commands(hostname)
+            
+            if not show_commands or show_commands is None:
+                logger.error(f"No show commands for hostname: {hostname}")
+                self.device_log_stats.update({hostname: []})
+                continue
+            
+            logger.info(f"Connection type: {conn_type}")
+            log_output = self.connect(hostname, config, show_commands)
+            
+            if not log_output:
+                logger.warning(f"No network logs generated for {hostname}.")
+                self.device_log_stats.update({hostname: log_output})
+                continue
+            
+            collection_flag = True
+            self.device_log_stats.update({hostname: log_output})
+            
+        if collection_flag:
+            return Const.LOG_COLL_GOOD
+        
+        return Const.LOG_COLL_SKIP
+            
+    def reset_device_log_stats(self):
+        self.device_log_stats = {}
     
     def compare_logs(self):
-        pass
+        if not self.pre_logs:
+            return Const.COMP_PRE_LOG_EMP
+        
+        if not self.post_logs:
+            return Const.COMP_POST_LOG_EMP
+        
+        parsed_pre_logs = LogComparisonManager.parse_log(self.pre_logs)
+        parsed_post_logs = LogComparisonManager.parse_log(self.post_logs)
+        self.comparison_result = LogComparisonManager.compare(parsed_pre_logs, parsed_post_logs)
+        
+        self.reset_logs()
+        
+        return Const.COMP_LOG_GOOD
     
-    def import_logs(self):
-        pass
+    def import_logs(self, folder_path: str, filenames: List[str]) -> Dict[str, List[str]]:
+        logger.info("Importing log files")
+        logger.info(f"Number of files to import: {len(filenames)}")
+        
+        host_with_logs = {}
+        
+        for filename in filenames:
+            hostname = self.extract_hostname(filename)
+            path = Path(folder_path) / filename
+            
+            try:
+                with open(path, mode="r") as file:
+                    data = file.read()
+                    host_with_logs.update({hostname: data})
+            except Exception as e:
+                logger.error(e)
+                
+        return host_with_logs
     
-    def extract_hostname(self):
-        pass
+    def extract_hostname(self, filename: str) -> str:
+        pattern = r"^(.+)_\d{8}_\d{6}\.log$"
+        match = re.match(pattern, filename)
+        
+        if match:
+            return match.group(1)
     
-    def connect(self):
-        pass
+    def connect(self, hostname: str, config: Dict[str, str], commands: List[str]):
+        logger.info(f"Connecting to {hostname}")
+        
+        try:
+            output = ""
+            net_connect = ConnectHandler(**config)
+            net_connect.enable()
+            
+            for cmd in commands:
+                logger.info(f"Executing command: {cmd}")
+                command_output = net_connect.send_command(cmd)
+                
+                if self.validate_log_output(command_output):
+                    output += f"{hostname}#{cmd}\n{command_output}\n\n"
+                else:
+                    msg = f"Invalid output from {cmd}: \n{hostname}#{cmd}\n{command_output}\n"
+                    logger.error(msg)
+                    
+            net_connect.disconnect()
+            logger.info(f"Connection to {hostname} closed successfully.")
+            return output
+        
+        except Exception as e:
+            logger.error(f"Error from {hostname}: {e}")
+            return
     
     def validate_log_output(self):
-        pass
-    
+        output = output.strip()
+        return "Invalid" not in output
+                
     def reset_logs(self):
-        pass
+        self.pre_logs = {}
+        self.post_logs = {}
     
     def get_hostname_with_logs(self):
-        pass
+        return dict(filter(lambda x: len(x[1]), self.device_log_stats.items()))
     
-    def store_network_logs(self):
-        pass
+    def store_network_logs(self, host_paths: Dict[str, str]):
+        if not self.device_log_stats:
+            logger.warning("No network logs available to compile.")
+            return
+        
+        if not host_paths:
+            logger.error("No host paths found.")
+            return
+        
+        flag = False
+        
+        for hostname, log in self.device_log_stats.items():
+            file_path = host_paths.get(hostname)
+            
+            if file_path is None:
+                logger.error(f"No host path found for {hostname}")
+                continue
+            
+            try:
+                with open(file_path, mode="w", encoding="utf-8") as file:
+                    file.write(log)
+                    flag = True
+                    
+                logger.info(f"Network logs stored for {hostname}")
+            
+            except Exception as e:
+                logger.error(e)
+                return Const.LOG_COLL_FILE_SAVE_BAD
+            
+        if not flag:
+            logger.error(f"Failed to store network logs.")
+            return Const.LOG_COLL_FILE_SAVE_BAD
+        
+        logger.info("Network logs stored successfully.")
+        return Const.LOG_COLL_FILE_SAVE_GOOD
+    
